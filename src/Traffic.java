ba08012c448c7b3a43fffcc5c9e890a96d9b2ae5
Armaan H <97925527+Armaanss@users.noreply.github.com>
2022-04-25 11:05:01 -0700

    Initial commit

---
+++ src/Traffic.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Traffic {
+	
+	JFrame frame = new JFrame("Traffic Simulation");
+	
+	public Traffic() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
