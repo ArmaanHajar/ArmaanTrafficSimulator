90e88db7073c8674e192d9506c4eef04f849da2e
Armaan H <97925527+Armaanss@users.noreply.github.com>
2022-05-03 11:15:28 -0700

    Final Project

--- src/Road.java
+++ src/Road.java
@@ -10,6 +10,7 @@ public class Road extends JPanel {
 	final int LANE_HEIGHT = 120;
 	final int ROAD_WIDTH = 800;
 	ArrayList<Vehicle> cars = new ArrayList<Vehicle>();
+	int carCount = 0;
 	
 	public Road() {
 		super();
@@ -38,25 +39,46 @@ public class Road extends JPanel {
 	public void step() {
 		for (int a = 0; a < cars.size(); a++) {
 			Vehicle v = cars.get(a);
-			v.setX(v.getX() + v.getSpeed());
-			if (v.getX() > ROAD_WIDTH) {
-				v.setX(0);
+			if (collision(v.getX() + v.getSpeed(), v.getY(), v) == false) { // theres no collision
+				v.setX(v.getX() + v.getSpeed());
+				if (v.getX() > ROAD_WIDTH) {
+					if (collision(0, v.getY(), v) == false) {
+						v.setX(0);
+						carCount++;
+					}
+				}	
+			}
+			else { // car ahead!
+				if ((v.getY() > 40) && (collision(v.getX(), v.getY() - LANE_HEIGHT, v) == false)) {
+						v.setY(v.getY() - LANE_HEIGHT);
+				}
+				else if ((v.getY() < 40 + 3 * LANE_HEIGHT) && (collision(v.getX(), v.getY() + LANE_HEIGHT, v) == false)) {
+					v.setY(v.getY() + LANE_HEIGHT);
+				}
 			}
 		}
 	}
 	
-	public boolean collision(int x, int y, int width, Vehicle v) {
+	public boolean collision(int x, int y, Vehicle v) {
 		for (int a = 0; a < cars.size(); a++) {
 			Vehicle u = cars.get(a);
 			if (y == u.getY()) { // if i'm in the same lane
 				if (u.equals(v) == false) { // if i'm not checking myself
-					if (v.getX() < u.getX() + u.getWidth() && // my left side is left of their right
-							v.getX() + v.getWidth() > u.getX()) { // my right side is right of their right
+					if (x < u.getX() + u.getWidth() && // my left side is left of their right
+							x + v.getWidth() > u.getX()) { // my right side is right of their right
 						return true;
 					}
 				}
 			}
 		}
 		return false;
+	}
+	
+	public int getCarCount() {
+		return carCount;
+	}
+	
+	public void resetCarCount() {
+		carCount = 0;
 	}
 }
--- src/Traffic.java
+++ src/Traffic.java
@@ -6,33 +6,37 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Traffic implements ActionListener, Runnable {
 	
 	JFrame frame = new JFrame("Traffic Simulation");
 	Road road = new Road();
-	// South Container
+	// south Container
 	JButton start = new JButton("Start");
 	JButton stop = new JButton("Stop");
+	JLabel throughput = new JLabel("Throughput: 0");
 	Container south = new Container();
-	// West Container
+	// west Container
 	JButton semi = new JButton ("Add Semi");
 	JButton suv = new JButton ("Add SUV");
 	JButton sports = new JButton ("Add Sports");
 	Container west = new Container();
-	
 	boolean running = false;
+	int carCount = 0;
+	long startTime = 0;
 
 	public Traffic() {
 		frame.setSize(1000, 550);
 		frame.setLayout(new BorderLayout());
 		frame.add(road, BorderLayout.CENTER);
 		
-		south.setLayout(new GridLayout(1, 2));
+		south.setLayout(new GridLayout(1, 3));
 		south.add(start);
 		start.addActionListener(this);
 		south.add(stop);
 		stop.addActionListener(this);
+		south.add(throughput);
 		frame.add(south, BorderLayout.SOUTH);
 		
 		west.setLayout(new GridLayout(3,1));
@@ -46,16 +50,6 @@ public class Traffic implements ActionListener, Runnable {
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
-		/*
-		Semi testSemi = new Semi(10, 20);
-		road.addCar(testSemi);
-		SUV testSUV = new SUV(10, 170);
-		road.addCar(testSUV);
-		Sports testSports = new Sports(10, 320);
-		road.addCar(testSports);
-		frame.repaint();
-		*/
 	}
 
 	public static void main(String[] args) {
@@ -67,6 +61,8 @@ public class Traffic implements ActionListener, Runnable {
 		if (event.getSource().equals(start)) {
 			if (running == false) {
 				running = true;
+				road.resetCarCount();
+				startTime = System.currentTimeMillis();
 				Thread t = new Thread(this);
 				t.start();
 			}
@@ -81,7 +77,7 @@ public class Traffic implements ActionListener, Runnable {
 				for (int y = 40; y < 480; y = y + 120) {
 					semi.setX(x);
 					semi.setY(y);
-					if (road.collision(x, y, semi.getWidth(), semi) == false) {
+					if (road.collision(x, y, semi) == false) {
 						frame.repaint();
 						return;
 					}
@@ -96,7 +92,7 @@ public class Traffic implements ActionListener, Runnable {
 				for (int y = 40; y < 480; y = y + 120) {
 					suv.setX(x);
 					suv.setY(y);
-					if (road.collision(x, y, suv.getWidth(), suv) == false) {
+					if (road.collision(x, y, suv) == false) {
 						frame.repaint();
 						return;
 					}
@@ -111,7 +107,7 @@ public class Traffic implements ActionListener, Runnable {
 				for (int y = 40; y < 480; y = y + 120) {
 					sports.setX(x);
 					sports.setY(y);
-					if (road.collision(x, y, sports.getWidth(), sports) == false) {
+					if (road.collision(x, y, sports) == false) {
 						frame.repaint();
 						return;
 					}
@@ -124,6 +120,9 @@ public class Traffic implements ActionListener, Runnable {
 	public void run() {
 		while (running == true) {
 			road.step();
+			carCount = road.getCarCount();
+			double throughputCalc = carCount / (1000 * (double)(System.currentTimeMillis() - startTime));
+			throughput.setText("Throughput: " + throughputCalc);
 			frame.repaint();
 			try {
 				Thread.sleep(100);
